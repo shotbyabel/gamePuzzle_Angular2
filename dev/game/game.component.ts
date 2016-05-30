17968a744452d4f6502bd9b39c3df347081cffe3
kimo5532 <shotbyabel@gmail.com>
2016-05-30 14:46:41 -0700

    finish basica game logic

--- dev/game/game.component.ts
+++ dev/game/game.component.ts
@@ -1,4 +1,5 @@
 import {Component} from 'angular2/core';
+import {OnInit} from 'angular2/core';
 
 @Component({
 
@@ -11,35 +12,68 @@ import {Component} from 'angular2/core';
         <input type="text" #name (keyup)="0">
        </section>
 
-
        <section 
-       [ngClass]="{game: true}" 
-       [style.display]="name.value === '' ? 'none' : 'block'">
-         <h4>DA GAME</h4>
-           <p>Hey, Welcome <span class="name">xxx</span></p>
+       [ngClass]="{
+
+         game: true,
+         solved: sw1.value == sw1Number && 
+                 sw2.value == sw2Number && 
+                 sw3.value == sw3Number && 
+                 sw4.value == sw4Number
+         }" 
+       
+
+       [ngStyle]="{display: name.value === '' ? 'none' : 'block'}">
+         
+         <h4>
+
+         DA GAME |
+         {{sw1.value == sw1Number && 
+                 sw2.value == sw2Number && 
+                 sw3.value == sw3Number && 
+                 sw4.value == sw4Number ? 'Solved!' : 'NOT solved!!'}}
+
+         </h4>
+         
+           <p>Hey, Welcome <span class="name">{{name.value}}</span></p>
             <br>
             SWITCH 1:
-              <input type="text" #sw1><br>
+              <input type="text" (keyup)="0" #sw1><br>
             SWITCH 2:
-              <input type="text" #sw2><br>
+              <input type="text" (keyup)="0" #sw2><br>
             SWITCH 3:
-              <input type="text" #sw3><br>
+              <input type="text" (keyup)="0" #sw3><br>
             SWITCH 4:
-              <input type="text" #sw4><br>                           
+              <input type="text" (keyup)="0" #sw4><br>                           
        </section> 
 
 
-       <h1>Way to go xxx, you did it!</h1>
+       <h1 [hidden]="sw1.value != sw1Number || 
+                 sw2.value != sw2Number || 
+                 sw3.value != sw3Number || 
+                 sw4.value != sw4Number" >Way to go {{name.value}}, you did it!</h1>
 
 `,
 })
 
-export class GameComponent {
+export class GameComponent implements OnInit {
 
   sw1Number: number;
   sw2Number: number;
   sw3Number: number;
   sw4Number: number;
+
+  ngOnInit() {
+    // return null;
+    this.sw1Number = Math.round(Math.random());
+    this.sw2Number = Math.round(Math.random());
+    this.sw3Number = Math.round(Math.random());
+    this.sw4Number = Math.round(Math.random());
+
+
+    console.log(this.sw1Number, this.sw2Number, this.sw3Number, this.sw4Number);
+  }
+
   
 }
 
